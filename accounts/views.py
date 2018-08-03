a4239882b0d7aba77b1da8143c487ead5d37d07e
kanishka-linux <kanishka.linux@gmail.com>
2018-08-03 12:39:19 +0530

    do not allow signup if not allowed in settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.conf import settings
+from django.http import HttpResponse
 
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('home')
+    if settings.ALLOW_ANY_ONE_SIGNUP:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                auth_login(request, user)
+                return redirect('home')
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
     else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        return HttpResponse('<html>Sorry! New Sign UP Not allowed. Ask moderator or Admin to create account for you.</html>');
