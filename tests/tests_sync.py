e55df2fc5af46196b7d9c085ac1c9261be6666ad
kanishka-linux <kanishka.linux@gmail.com>
2018-08-16 15:47:25 +0530

    add test_check_url

--- tests/tests_sync.py
+++ tests/tests_sync.py
@@ -1,15 +1,24 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import resolve, reverse
 from pages.models import Library
 from pages.views import dashboard
+from django.utils import timezone
 
 
 class LibraryTests(TestCase):
     
-    def setUp(self):
+    client = Client()
+    url = 'https://en.wikipedia.org/wiki/Main_Page'
+    
+    @classmethod
+    def setUpTestData(cls):
         usr = User.objects.create_user(username='johndoe', password='clrsalgo')
         Library.objects.create(usr=usr, directory='TMP')
+        Library.objects.create(usr=usr, directory='TMP',
+                               url=cls.url, timestamp=timezone.now())
+    
+    def setUp(self):
         self.client.login(username='johndoe', password='clrsalgo')
 
     def test_dashboard_page(self):
@@ -21,5 +30,10 @@ class LibraryTests(TestCase):
         url = reverse('home_page', kwargs={'username': 'johndoe'})
         response = self.client.post(url, {'create_directory':'Sample'})
         self.assertEquals(response.status_code, 200)
+        
+    def test_check_url(self):
+        url = reverse('navigate_directory', kwargs={'username': 'johndoe', 'directory': 'TMP'})
+        response = self.client.get(url)
+        self.assertContains(response, self.url)
     
     
