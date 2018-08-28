251e2c0f7808998af1cc4eeb214c46fd9bff0e78
kanishka-linux <kanishka.linux@gmail.com>
2018-08-28 22:30:41 +0530

    add license header

--- pages/urls.py
+++ pages/urls.py
@@ -1,3 +1,22 @@
+"""
+Copyright (C) 2018 kanishka-linux kanishka.linux@gmail.com
+
+This file is part of Reminiscence.
+
+Reminiscence is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reminiscence is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Reminiscence.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from django.urls import path
 from django.urls import re_path as url
 from .views import *
