f8511adb5c3cedd4b327409295a2eb7e549a8594
kanishka-linux <kanishka.linux@gmail.com>
2018-09-08 15:04:40 +0530

    add title and version info

--- reminiscence/__init__.py
+++ reminiscence/__init__.py
@@ -3,3 +3,9 @@ from __future__ import absolute_import
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+
+__title__ = 'Reminiscence: Self-hosted bookmark and archive manager'
+__version__ = '0.1'
+__author__ = 'kanishka-linux (AAK)'
+__license__ = 'AGPLv3'
+__copyright__ = 'Copyright (C) 2018 kanishka-linux (AAK) kanishka.linux@gmail.com'
