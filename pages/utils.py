4d8f1ed00d40792473633fd993b3f976ed4c0cd2
kanishka-linux <kanishka.linux@gmail.com>
2018-09-28 13:30:27 +0530

    add RangeFileResponse class

--- pages/utils.py
+++ pages/utils.py
@@ -184,3 +184,34 @@ class ImportBookmarks:
         return links_dict
     
 
+#https://stackoverflow.com/questions/33208849/python-django-streaming-video-mp4-file-using-httpresponse
+
+class RangeFileResponse:
+    
+    def __init__(self, filelike, blksize=8192, offset=0, length=None):
+        self.filelike = filelike
+        self.filelike.seek(offset, os.SEEK_SET)
+        self.remaining = length
+        self.blksize = blksize
+
+    def close(self):
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
